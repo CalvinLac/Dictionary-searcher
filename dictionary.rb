0ecea8430123409c5d290dbf1c726ed4030e0779
Calvin <Calvin.lacc@gmail.com>
2016-07-06 14:23:30 -0600

    Finished added the four search functions

--- dictionary.rb
+++ dictionary.rb
@@ -10,7 +10,7 @@ class DictionaryLoader
 		end
 		return word_bank
 	end
-	end
+end
 
 
 
@@ -21,13 +21,104 @@ class DictionaryUI
 	def initialize
 		@dictionary_loader = DictionaryLoader.new
 		@main_dictionary = @dictionary_loader.loading('5desk.txt')
-		puts @main_dictionary
 	end
+
+
+	def exact_match (search,dictionary)
+		result = []
+
+		dictionary.each do |word|
+			result << word if word.match(/^#{search}$/)
+ 		end
+ 		if result.size == 0
+ 			puts "There was no matches found"
+ 		end
+ 		result
+ 	end
+
+	def part_match (search,dictionary)
+		result = []
+
+		dictionary.each do |word|
+			result << word if word.match(/^.*#{search}.*$/)
+ 		end
+ 		if result.size == 0
+ 			puts "There was no matches found"
+ 		end
+ 		result
+ 	end
+
+	def begins_match (search,dictionary)
+		result = []
+
+		dictionary.each do |word|
+			result << word if word.match(/^#{search}.*$/)
+ 		end
+ 		if result.size == 0
+ 			puts "There was no matches found"
+ 		end
+ 		result
+ 	end
+
+ 	def ends_match (search,dictionary)
+		result = []
+
+		dictionary.each do |word|
+			result << word if word.match(/^.*#{search}$/)
+ 		end
+ 		if result.size == 0
+ 			puts "There was no matches found"
+ 		end
+ 		result
+ 	end
+
+
+ 	def what_mode
+ 		puts "Pick your mode"
+ 		puts "1 for exact"
+ 		puts "2 for partial" 
+ 		puts "3 for begins-with"
+ 		puts "4 for ends-with"
+ 		mode = gets.chomp
+ 		return mode
+ 	end
+
+
+
+ 	def displayer
+ 		mode = what_mode.to_i
+ 		puts "Please enter your search word or string to search"
+ 		searcher = gets.chomp 
+ 		dictionary= @main_dictionary
+
+ 		if mode ==1 
+ 		puts exact_match(searcher,dictionary)
+
+ 		elsif mode ==2
+ 		puts part_match(searcher,dictionary)
+
+ 		elsif mode ==3
+ 		puts begins_match(searcher,dictionary)
+
+ 		elsif mode ==4
+ 		puts ends_match(searcher,dictionary)
+ 	end
+
+ 			
+ 	end
+
 
 end
 
-d = DictionaryUI.new 
 
+
+
+
+
+
+
+d = DictionaryUI.new 
+d.displayer
 
 
 
